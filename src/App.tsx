55b5b2d46602c89fc0ed06e1e3074e0558c40925
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-02-06 15:02:54 +0200

    creating types and fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,35 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+// components
+import { Drawer, LinearProgress, Grid, Badge } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+//styles
+import { Wrapper } from './App.styles';
+
+//types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: string;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch('https://fakestoreapi.com/products')).json();
+};
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  );
+  console.log(data);
+
   return <div className="App">Start</div>;
 };
 
